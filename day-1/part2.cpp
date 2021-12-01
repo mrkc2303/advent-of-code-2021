6459504e556553110f9c9c1ad715c47919053fb4
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2021-12-02 00:30:39 +0530

    Create part2.cpp

---
+++ day-1/part2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10000] = {0};
+	int n, i=0, count=0, previous = 1e7, size=0; 
+	while (cin >> n) {
+	    arr[i] = n;
+	    i++;
+	}
+	size = i;
+    i=0;
+	while(i < size) {
+	   if ((arr[i+1] + arr[i+2] + arr[i+3]) > (arr[i] + arr[i+1] + arr[i+2])) 
+	        count++;
+	   i++;
+	}
+	cout << count;
+	return 0;
+}
